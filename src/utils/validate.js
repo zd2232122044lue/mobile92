f08eda0c7c89797fa53111a3d551199732d5e079
小孩 <2232122044@qq.com>
2020-02-11 21:48:58 +0800

    登录-表单校验-中文错误信息

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,16 @@
-import { extend } from 'vee-validate'
+// 1.导入localize模块函数
+import { extend,localize } from 'vee-validate'
 // 导入全部的规则
 import * as rules from 'vee-validate/dist/rules'
+
+// 2.导入要使用的语言
+import zhCN from 'vee-validate/dist/locale/zh_CN.json'
+// 3.对导入好的语言做注册
+localize({
+    zhCN // 对象成员简易赋值
+})
+// 4.使用具体的语言
+localize('zhCN')
 
 // 注册全部的规则
 Object.keys(rules).forEach(rule => {
