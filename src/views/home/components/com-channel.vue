0f35b5c98541ab75b23de20ec21ecfedf18a4f23
小孩 <2232122044@qq.com>
2020-02-15 21:50:58 +0800

    频道订制管理-弹出层显示与隐藏

---
+++ src/views/home/components/com-channel.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- van-popup 弹出层容器,用于展示弹窗,信息提示等内容,支持多个弹出层叠加展示
+        v-model="Boolean变量": 设置弹出层是否显示
+            现在通过:value和@input体现出来,因为具体的控制开关
+        closeable: 是否显示关闭图标
+        round: 是否显示圆角
+        position: 弹出位置,可选值为 top bottom right left
+        :style="{height: '20%'}": 弹出层高度
+        close-icon-position: 关闭图标位置,可选值为top-left bottom-left bottom-right
+-->
+  <van-popup
+    :value="value"
+    @input="$emit('input',$event)"
+    closeable
+    round
+    position="bottom"
+    :style="{height: '20%'}"
+    close-icon-position="top-left"
+  >
+  </van-popup>
+</template>
+
+<script>
+export default {
+  name: 'com-channel',
+  props: {
+    // 接收父组件v-model传递过来的数据信息
+    value: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,45 +1,52 @@
 <template>
   <div class="container">
     <!-- van-tab/van-tabs: 标签页组件
-      v-model: 通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签
-      title: 标题
+          v-model: 通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签
+          title: 标题
     -->
     <van-tabs v-model="activeChannelIndex">
       <!-- 给标签页设置图标,用法是命名插槽
               slot="nav-right/right" 图标放置的位置
               class="channel-more" 设置css样式
       -->
-      <div slot="nav-right" class="channel-more">
-         <!-- 图标  name="wap-nav" 3杠图标 -->
+      <div slot="nav-right" class="channel-more" @click="showChannel=true">
+        <!-- 图标  name="wap-nav" 3杠图标 -->
         <van-icon name="wap-nav" />
       </div>
+      <!-- 使用频道弹出层组件 -->
+      <com-channel v-model="showChannel"></com-channel>
       <!-- <van-tab title="推荐"> -->
       <van-tab :title="item.name" v-for="item in channelList" :key="item.id">
         <!-- 使用文章列表组件 -->
-          <!-- 获取文章列表,需要频道id条件,因此在父组件中要把当前频道id作为参数传递给子组件 -->
-          <!-- 父组件(home/index.vue): 通过属性值方式传递频道id到子组件 -->
+        <!-- 获取文章列表,需要频道id条件,因此在父组件中要把当前频道id作为参数传递给子组件 -->
+        <!-- 父组件(home/index.vue): 通过属性值方式传递频道id到子组件 -->
         <com-article :channelID="item.id"></com-article>
       </van-tab>
       <!-- <van-tab title="数据库">数据库内容展示</van-tab>
-      <van-tab title="后端">后端内容展示</van-tab> -->
+      <van-tab title="后端">后端内容展示</van-tab>-->
     </van-tabs>
   </div>
 </template>
 
 <script>
+// 导入频道弹出层组件
+import ComChannel from './components/com-channel';
 // 导入文章列表组件
-import ComArticle from './components/com-article'
+import ComArticle from './components/com-article';
 // 导入频道列表api函数
-import { apiChannelList } from '@/api/channel.js'
+import { apiChannelList } from '@/api/channel.js';
 
 export default {
   name: 'home-index',
-  // 注册文章列表组件
+  // 注册组件
   components: {
-    ComArticle
+    ComArticle,
+    ComChannel
   },
   data () {
     return {
+      // 频道操作弹出层组件显示标志
+      showChannel: false,
       // 用户频道列表
       channelList: [],
       // 激活频道下标标志
@@ -64,15 +71,15 @@ export default {
 
 <style scoped lang='less'>
 // 给 更多 频道获得样式
-.channel-more{
-  position:fixed;
-  right:0;
+.channel-more {
+  position: fixed;
+  right: 0;
   background-color: white;
-  line-height:88px;
-  height:88px;
+  line-height: 88px;
+  height: 88px;
   width: 90px;
-  text-align:center;
-  font-size:40px;
+  text-align: center;
+  font-size: 40px;
 }
 
 // 频道列表
@@ -89,7 +96,6 @@ export default {
   }
   /deep/ .van-tab__pane {
     height: 100%;
-
   }
   // 给频道下边沿横向设置样式
   /deep/ .van-tabs__line {
