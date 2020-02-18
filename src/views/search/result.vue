d1013badf74df5e88c6b3a7ec08477c99fb7596f
小孩 <2232122044@qq.com>
2020-02-18 18:29:06 +0800

    文章搜索结果页面-页面布局

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,6 +1,14 @@
 <template>
   <div class='container'>
-    <van-nav-bar title="搜索结果" left-arrow @click-left="$router.back()" />
+    <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+    <van-list 
+      v-model="loading" 
+      :finished="finished" 
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item" /> 
+    </van-list>
   </div>
 </template>
 
@@ -17,7 +25,11 @@ export default {
   },
   data(){
     return {
-      searchList: [] // 搜索结果
+      searchList: [], // 搜索结果
+      // 瀑布流相关
+      list: [],
+      loading: false,
+      finished: false
     }
   },
   created(){
@@ -29,9 +41,39 @@ export default {
     async getSearchList(){
       let result = await apiSearchList({q:this.q})
       this.searchList = result.results
-    }
+    },
+
+    // 瀑布加载
+    onLoad(){
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+    }, 1000)
+   }
   }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style lang="less" scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  box-sizing: border-box;
+  .van-list {
+    flex: 1;
+    margin-top: 92px;
+  }
+}
+</style>
