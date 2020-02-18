727509fc2a13b7eb6105415c227255569306ef67
小孩 <2232122044@qq.com>
2020-02-18 18:37:19 +0800

    文章搜索结果页面-渲染真实数据

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -7,7 +7,7 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" /> 
+      <van-cell v-for="item in searchList" :key="item.art_id.toString()" :title="item.title" /> 
     </van-list>
   </div>
 </template>
@@ -25,6 +25,8 @@ export default {
   },
   data(){
     return {
+      page: 1, // 页码
+      per_page: 10, // 条数
       searchList: [], // 搜索结果
       // 瀑布流相关
       list: [],
@@ -32,33 +34,50 @@ export default {
       finished: false
     }
   },
-  created(){
-    // 获得搜索结果
-    this.getSearchList()
-  },
+  // created(){
+  //   // 获得搜索结果
+  //   this.getSearchList()
+  // },
   methods: {
-    // 获得搜索结果
-    async getSearchList(){
-      let result = await apiSearchList({q:this.q})
-      this.searchList = result.results
-    },
+    // // 获得搜索结果
+    // async getSearchList(){
+    //   let result = await apiSearchList({q:this.q})
+    //   this.searchList = result.results
+    // },
 
     // 瀑布加载
-    onLoad(){
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
+    async onLoad(){
+      await this.$sleep(1000) // 延迟一秒
 
+      let result = await apiSearchList({ q:this.q, page: this.page, per_page:this.per_page})
+      // 加载状态结束
+      this.loading = false
+      if(!result.results.length){
         // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-    }, 1000)
+        this.finished = true
+        return false
+      }
+      // 追加数据
+      this.searchList.push(...result.results)
+      // 页码累加
+      this.page++
+
+
+    //    onLoad(){
+    //   // 异步更新数据
+    //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+    //   setTimeout(() => {
+    //     for (let i = 0; i < 10; i++) {
+    //       this.list.push(this.list.length + 1)
+    //     }
+    //     // 加载状态结束
+    //     this.loading = false
+
+    //     // 数据全部加载完成
+    //     if (this.list.length >= 40) {
+    //       this.finished = true
+    //     }
+    // }, 1000)
    }
   }
 }
