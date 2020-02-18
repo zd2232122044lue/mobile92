e8790dfd5b1e3cfc6e9b77854d61d0ba921fe75a
小孩 <2232122044@qq.com>
2020-02-18 17:47:54 +0800

    文章搜索结果-获取搜索数据

--- src/api/search.js
+++ src/api/search.js
@@ -12,3 +12,20 @@ export function apiSuggestionList ({ q }) {
     }
   })
 }
+
+// 根据联想关键字搜索文章
+// 参数: q 联想的关键字
+//       page 页码
+//       per_page 返回内容条数
+//   page = 1, per_page = 10 对象结构赋值: 给参数设置默认值
+export function apiSearchList ({ q, page = 1, per_page = 10 }) {
+  return request({
+    url: '/app/v1_0/search',
+    method: 'get',
+    params: {
+      q,
+      page,
+      per_page
+    }
+  })
+}
--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -5,8 +5,32 @@
 </template>
 
 <script>
+// 导入根据联想关键字搜索文章的api
+import { apiSearchList } from '@/api/search'
 export default {
-  name: 'search-result'
+  name: 'search-result',
+  computed:{
+    // 获得路由参数q,使得组件内部可以通过this.q的简便方式获得路由参数
+    q(){
+      return this.$route.params.q
+    }
+  },
+  data(){
+    return {
+      searchList: [] // 搜索结果
+    }
+  },
+  created(){
+    // 获得搜索结果
+    this.getSearchList()
+  },
+  methods: {
+    // 获得搜索结果
+    async getSearchList(){
+      let result = await apiSearchList({q:this.q})
+      this.searchList = result.results
+    }
+  }
 }
 </script>
 
