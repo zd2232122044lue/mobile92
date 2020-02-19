dfacbeb52acd7679e396e9cc505781b25133688c
小孩 <2232122044@qq.com>
2020-02-19 23:19:05 +0800

    个人中心-页面布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -4,9 +4,21 @@
      title: 导航栏 中间信息
      right-text: 右侧信息
      fixed: 导航栏 固定在头部
-    @click-right: 右侧文字单击事件-->
-    <van-nav-bar fixed title="黑马头条" right-text="搜索" @click-right="$router.push('/search')" />
-    <div class="my-wrapper">
+     @click-right: 右侧文字单击事件
+     v-if="showNavBar": 如果是个人中心,就不显示头部导航栏
+    -->
+    <van-nav-bar
+      fixed
+      title="黑马头条"
+      right-text="搜索"
+      @click-right="$router.push('/search')"
+      v-if="showNavBar"
+    />
+    <!-- noTop：设置padding-top为0，只有用户中心页面需要设置，其他子页码不要设置
+          showNavBar:false===>个人中心====>额外增加noTop
+          showNavBar:true===>其他===>没有noTop
+    -->
+    <div class="my-wrapper" :class="{noTop:!showNavBar}">
       <router-view></router-view>
     </div>
     <!-- 标签栏 (页面底部显示(本身有样式设置))
@@ -33,6 +45,13 @@ export default {
     // 判断用户是否登录系统,返回不同的路由执行地址
     userGo: function () {
       return this.$store.state.user.token ? '/user' : '/login'
+    },
+    // 判断是否正在显示个人中心,是返回false,不是返回true
+    //  对应的导航栏据此设置是否显示
+    showNavBar () {
+      // this 指向组件实例
+      // this.$route.path 感知路由地址信息
+      return this.$route.path !== '/user'
     }
   }
 }
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,5 +1,58 @@
 <template>
-  <div class='container'>User</div>
+  <div class='container'>
+     <div class="user-profile">
+      <div class="info">
+        <van-image round src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <h3 class="name">
+          我是华仔
+          <br />
+          <van-tag size="mini">申请认证</van-tag>
+        </h3>
+      </div>
+      <!--
+        van-row/van-col：布局组件
+        row:设置行
+        col:设置列
+          span设置列的宽度，总数24
+      -->
+      <van-row>
+        <van-col span="6">
+          <p>0</p>
+          <p>动态</p>
+        </van-col>
+        <van-col span="6">
+          <p>0</p>
+          <p>关注</p>
+        </van-col>
+        <van-col span="6">
+          <p>0</p>
+          <p>粉丝</p>
+        </van-col>
+        <van-col span="6">
+          <p>0</p>
+          <p>被赞</p>
+        </van-col>
+      </van-row>
+    </div>
+    <van-row class="user-links">
+      <van-col span="8">
+        <van-icon name="newspaper-o" color="#7af"/>我的作品
+      </van-col>
+      <van-col span="8">
+        <van-icon name="star-o" color="#f00"/>我的收藏
+      </van-col>
+      <van-col span="8">
+        <van-icon name="tosend" color="#fa0"/>阅读历史
+      </van-col>
+    </van-row>
+
+    <van-cell-group class="user-group">
+      <van-cell icon="edit" title="编辑资料" to="/user/profile" is-link />
+      <van-cell icon="chat-o" title="小智同学" to="/user/chat" is-link />
+      <van-cell icon="setting-o" title="系统设置" is-link />
+      <van-cell icon="warning-o" title="退出登录" to="/login" is-link />
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
@@ -8,4 +61,61 @@ export default {
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+/**
+&:并且，做连接使用
+.user{
+  &-profile{color:red}
+  &-pear{}
+  apple{}
+}
+解析：.user-profile{color:red}
+      .user-pear{}
+      .user apple{}
+*/
+.user {
+  &-profile {
+    width: 100%;
+    height: 300px;
+    display: block;
+    background: #3296fa;
+    color: #fff;
+    .info {
+      display: flex;
+      padding: 40px;
+      align-items: center;
+      .van-image{
+        width: 128px;
+        height: 128px;
+      }
+      .name {
+        font-size: 32px;
+        font-weight: normal;
+        margin-left: 20px;
+      }
+      .van-tag {
+        background: #fff;
+        color: #3296fa;
+      }
+    }
+    p{
+      margin: 0;
+      text-align: center;
+    }
+  }
+  &-group {
+    margin-bottom: 30px;
+  }
+  &-links {
+    padding: 30px 0;
+    font-size: 24px;
+    text-align: center;
+    background-color: #fff;
+    .van-icon {
+      display: block;
+      font-size: 48px;
+      padding-bottom: 10px;
+    }
+  }
+}
+</style>
