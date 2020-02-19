2eb88e5766e8b3c6ec6d5df212d51fb2fca693f5
小孩 <2232122044@qq.com>
2020-02-19 23:44:56 +0800

    个人中心-编辑资料-页面布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -7,19 +7,38 @@
       right-text="保存"
       @click-right="save()"
     ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像" center>
+        <!--
+        slot="default" 自定义单元格右侧内容
+        fit="cover"  图片填充模式 保持宽高缩放图片，使图片的短边能完全显示出来，裁剪长边
+        -->
+        <van-image
+          slot="default"
+          width="56"
+          height="56"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <!-- value=xxx 设置单元格右侧内容部分 -->
+      <van-cell is-link title="名称" value="用户名称"/>
+      <van-cell is-link title="性别" value="男"/>
+      <van-cell is-link title="生日" value="2019-08-08"/>
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: "user-profile",
+  name: 'user-profile',
   methods: {
-    save() {
-      // 提示信息
-      this.$toast.success("保存成功");
+    save () {
+      this.$toast.success('保存成功')
     }
   }
-};
+}
 </script>
 
 <style scoped lang='less'></style>
