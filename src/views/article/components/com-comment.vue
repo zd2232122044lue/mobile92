db5364d539d38ea432d7cb016e64e50f931a0173
小孩 <2232122044@qq.com>
2020-02-19 20:44:07 +0800

    文章详情页面-回复-页面布局

--- src/views/article/components/com-comment.vue
+++ src/views/article/components/com-comment.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="comment">
     <!--van-list：实现瀑布加载效果-->
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了"
-              @load="onLoad">
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
       <van-cell v-for="item in commentList" :key="item.com_id.toString()">
         <!-- 作者头像 -->
         <div slot="icon">
-          <img class="avatar"
-               :src="item.aut_photo" alt="">
+          <img class="avatar" :src="item.aut_photo" alt />
         </div>
         <!-- 作者名称 -->
         <div slot="title">
@@ -15,11 +13,14 @@
         </div>
         <!-- 点赞 -->
         <div slot="default">
-            <!-- type="item.is_liking?'danger':'default'"
-                设置当前用户是否点赞,来设置不同的样式 -->
-          <van-button icon="like-o" size="mini" plain
+          <!-- type="item.is_liking?'danger':'default'"
+          设置当前用户是否点赞,来设置不同的样式-->
+          <van-button
+            icon="like-o"
+            size="mini"
+            plain
             type="item.is_liking?'danger':'default'"
-          > &nbsp;{{item.like_count}} </van-button>
+          >&nbsp;{{item.like_count}}</van-button>
         </div>
         <!-- 评论内容和时间 -->
         <div slot="label">
@@ -27,11 +28,40 @@
           <p>
             <span>{{item.pubdate | formatTime}}</span>
             ·
-            <span>{{item.reply_count}}&nbsp;回复</span>
+            <span @click="showReply=true">{{item.reply_count}}&nbsp;回复</span>
           </p>
         </div>
       </van-cell>
     </van-list>
+
+    <!-- 回复列表展示-弹出层/瀑布 -->
+    <van-popup v-model="showReply" position="bottom" :style="{ height: '80%' }" round>
+      <!-- 瀑布加载效果 -->
+      <van-list
+        v-model="reply.loading"
+        :finished="reply.finished"
+        finished-text="没有更多了"
+        @load="onLoadReply"
+      >
+        <van-cell v-for="item in reply.list" :key="item" :title="item">
+          <!-- 作者头像 -->
+          <div slot="icon">
+            <img class="avatar" src="http://toutiao.meiduo.site/Fn6-mrb5zLTZIRG3yH3jG8HrURdU" alt />
+          </div>
+          <!-- 作者名称 -->
+          <div slot="title">
+            <span>度娘</span>
+          </div>
+          <!-- 评论内容和时间 -->
+          <div slot="label">
+            <p>好厉害呀</p>
+            <p>
+              <span>2019-12-30 15:15:15</span>
+            </p>
+          </div>
+        </van-cell>
+      </van-list>
+    </van-popup>
   </div>
 </template>
 <script>
@@ -54,11 +84,18 @@ export default {
       finished: false,
       // 评论相关
       commentList: [], // 评论列表
-      commentID: null // 评论分页标志
+      commentID: null, // 评论分页标志
+      // 回复相关
+      showReply: false, // 回复弹出层是否展示
+      reply: {
+        list: [],
+        loading: false,
+        finished: false
+      }
     }
   },
   methods: {
-    // 瀑布流加载
+    // 评论瀑布流加载
     async onLoad () {
       await this.$sleep(800) // 暂停0.8s
       // 获取评论列表
@@ -79,34 +116,53 @@ export default {
       // 接收commentID
       this.commentID = result.last_id
 
-    //   // 异步更新数据
-    //   setTimeout(() => {
-    //     for (let i = 0; i < 5; i++) {
-    //       this.list.push(this.list.length + 1)
-    //     }
-    //     // 加载状态结束
-    //     this.loading = false
-    //     // 数据全部加载完成
-    //     if (this.list.length >= 10) {
-    //       this.finished = true
-    //     }
-    //   }, 500)
+      //   // 异步更新数据
+      //   setTimeout(() => {
+      //     for (let i = 0; i < 5; i++) {
+      //       this.list.push(this.list.length + 1)
+      //     }
+      //     // 加载状态结束
+      //     this.loading = false
+      //     // 数据全部加载完成
+      //     if (this.list.length >= 10) {
+      //       this.finished = true
+      //     }
+      //   }, 500)
+    },
+
+    // 回复瀑布流加载
+    onLoadReply () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.reply.list.push(this.reply.list.length + 1)
+        }
+
+        // 加载状态结束
+        this.reply.loading = false
+
+        // 数据全部加载完成
+        if (this.reply.list.length >= 40) {
+          this.reply.finished = true
+        }
+      }, 1000)
     }
   }
 }
 </script>
 <style lang="less" scoped>
-.comment{
-  margin-top:15px;
+.comment {
+  margin-top: 15px;
   .avatar {
     width: 50px;
     height: 50px;
     border-radius: 100%;
     margin-right: 10px;
   }
-  /deep/ .van-cell__title{
-    .van-cell__label{
-      width:400px;
+  /deep/ .van-cell__title {
+    .van-cell__label {
+      width: 400px;
     }
   }
 }
