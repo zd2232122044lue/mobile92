d828841fadc6a711bc0201318b155aef8e9992e5
小孩 <2232122044@qq.com>
2020-02-22 19:36:47 +0800

    个人中心-编辑资料-弹出选择生日

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -25,7 +25,7 @@
       <!-- value=xxx 设置单元格右侧内容部分 -->
       <van-cell is-link title="名称" :value="userProfile.name" @click="showName=true"/>
       <van-cell is-link title="性别" :value='userProfile.gender === 0 ? "男" : "女"' @click="showSex=true"/>
-      <van-cell is-link title="生日" :value="userProfile.birthday" />
+      <van-cell is-link title="生日" :value="userProfile.birthday"  @click="showBirthday = true"/>
     </van-cell-group>
     <!-- 弹出选择头像 -->
     <van-popup v-model="showPhoto" position="bottom">
@@ -50,12 +50,31 @@
       :actions="actions"
       cancel-text="取消"
     ></van-action-sheet>
+    <!-- 弹出生日 -->
+    <van-popup v-model="showBirthday" position="bottom">
+      <!-- van-datetime-picker 时间选择器
+                v-model="nowDate": 默认激活显示时间
+                type="date": 时间类型 年月日
+                :min-date="minDate": 允许选取的最小时间
+                @cancel="showBirthday=false": 单击取消按钮回调时间
+                @confirm="confirmDate": 确认选取时间回调
+      -->
+      <van-datetime-picker
+        v-model="nowDate"
+        type="date"
+        :min-date="minDate"
+        @cancel="showBirthday=false"
+        @confirm="confirmDate"
+      ></van-datetime-picker>
+    </van-popup>
   </div>
 </template>
 
 <script>
 // 导入获取用户资料的api
 import { apiUserProfile } from '@/api/user.js'
+// 导入dayjs模块
+import dayjs from 'dayjs'
 
 export default {
   name: 'user-profile',
@@ -65,6 +84,9 @@ export default {
       showName: false, // 是否显示编辑昵称弹层
       showSex: false, // 是否显示选择性别弹层
       actions: [{ name: '男' }, { name: '女' }], // 弹出菜单项目定义
+      nowDate: new Date(), // 当前时间
+      minDate: new Date('1900-01-01'), // 允许选取的最小时间
+      showBirthday: false, // 是否显示生日弹层
       // 用户资料表单对象
       userProfile: {
         photo: '',
@@ -78,6 +100,22 @@ export default {
     this.getUserProfile() // 调用获取用户资料的方法
   },
   methods: {
+    // 时间选择器 被单击“确定”按钮后的回调处理
+    // val:固定代表选择好的时间信息
+    confirmDate (val) {
+      // console.log(val) // Thu May 21 2020 00:00:00 GMT+0800 (中国标准时间)
+      // console.log(typeof val) // object
+
+      // console.log(dayjs(val)) // dayjs时间对象，可以调用format等方法
+
+      // 要把获得到的对象时间变为“年-月-日”格式的时间
+
+      // 转化时间
+      this.userProfile.birthday = dayjs(val).format('YYYY-MM-DD')
+      // 关闭弹出层
+      this.showBirthday = false
+    },
+
     // 性别被选中回调处理
     //  val: 被选中项目的菜单单元信息  {name:xx}
     onSelect (val) {
@@ -90,6 +128,8 @@ export default {
     // 获取用户资料
     async getUserProfile () {
       this.userProfile = await apiUserProfile()
+      // 把获得好的用户生日信息变为对象格式,赋予给时间选取器的nowDate
+      this.nowDate = new Date(this.userProfile.birthday)
     },
 
     save () {
